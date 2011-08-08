b20709dd295e2512e09673cfd29e4c69635b01c3
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2011-08-08 22:33:31 +0200

    Exercise 10 initial commit

---
+++ 010-mardi-gras/mardi_gras.py
@@ -0,0 +1,11 @@
+# Mardi Gras
+# Compute the date of Easter
+# Programming Praxis Exercise 10
+# http://programmingpraxis.com/2009/02/24/mardi-gras/
+
+
+
+
+if __name__ == '__main__':
+    pass
+
---
+++ 010-mardi-gras/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MardiGrasTestCase(unittest.TestCase):
+    def test_programming_praxis(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
