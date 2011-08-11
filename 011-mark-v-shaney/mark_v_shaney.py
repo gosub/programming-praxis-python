f307bbd396a26b1867b438944cae8874926fedbb
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2011-08-11 15:35:55 +0200

    Exercise 11 initial commit

---
+++ 011-mark-v-shaney/mark_v_shaney.py
@@ -0,0 +1,11 @@
+# Mark V. Shaney
+# Generate parodies of a text using a Markov chain
+# Programming Praxis Exercise 11
+# http://programmingpraxis.com/2009/02/27/mark-v-shaney/
+
+
+
+
+if __name__ == '__main__':
+    pass
+
---
+++ 011-mark-v-shaney/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MarkovChainTestCase(unittest.TestCase):
+    def test_programming_praxis(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
