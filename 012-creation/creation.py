88fedb46b158cab063236f346f12800b988952e5
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2011-08-13 10:01:53 +0200

    Exercise 12 initial commit

---
+++ 012-creation/creation.py
@@ -0,0 +1,11 @@
+# Creation
+# Cryptanalysis of a Vigenère cipher -- encoding: utf-8
+# Programming Praxis Exercise 12
+# http://programmingpraxis.com/2009/03/03/creation/
+
+
+
+
+if __name__ == '__main__':
+    pass
+
