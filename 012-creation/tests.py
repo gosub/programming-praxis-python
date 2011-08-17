8ebd1d422e52e2f34bf47f9404428dd71f11436d
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2011-08-17 11:19:29 +0200

    Tests for exercise 12

--- 012-creation/tests.py
+++ 012-creation/tests.py
@@ -1,10 +1,25 @@
 import unittest
+import creation
 
 
 class CreationTestCase(unittest.TestCase):
-    def test_programming_praxis(self):
-        pass
+    def setUp(self):
+        self.encrypted = creation.read_cipher("cipher_message.txt")
+        self.decrypted = open("decipher_message.txt").read().rstrip('\n')
+        self.password = "Genesis"
 
+    def test_programming_praxis(self):
+        "Verify found password length, found password and decrypted message. "
+        passw_lens = creation.probable_password_lengths(self.encrypted)
+        passw_len = passw_lens[0]
+        self.assertTrue(passw_len % len(self.password) == 0)
+
+        found_passw = creation.guess_password(self.encrypted, passw_len)
+        possible_password_match = [self.password * i for i in [1, 2, 3]]
+        self.assertIn(found_passw, possible_password_match)
+
+        decrypt = creation.xor_crypt(self.encrypted, found_passw)
+        self.assertEqual(self.decrypted, decrypt)
 
 
 if __name__ == '__main__':
