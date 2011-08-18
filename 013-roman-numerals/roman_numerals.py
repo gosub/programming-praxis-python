edeb373e431db034fe4897a5f68dfdc04710963b
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2011-08-18 16:17:33 +0200

    Exercise 13 initial commit

---
+++ 013-roman-numerals/roman_numerals.py
@@ -0,0 +1,11 @@
+# Roman Numerals
+# A library to read and write Roman numerals
+# Programming Praxis Exercise 13
+# http://programmingpraxis.com/2009/03/06/roman-numerals/
+
+
+
+
+if __name__ == '__main__':
+    pass
+
